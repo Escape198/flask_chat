39f75344be7a8752b4b10c474bdcf3132789b0bc
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-29 22:02:38 +0300

    Delete forms.py

--- flask/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm as Form
-from wtforms.fields import StringField, SubmitField
-from wtforms import validators
-
-class LoginForm(Form):
-    name = StringField('Name', validators=[validators.DataRequired()])
-    room = StringField('Room', validators=[validators.DataRequired()])
-    submit = SubmitField('Enter Chatroom')
